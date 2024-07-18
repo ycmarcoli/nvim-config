8e40022e133a7a66a1bee6cbd22bf0365faee03d
Marco Li <y2996li@uwaterloo.ca>
2024-07-17 19:32:02 -0700

    Adds some color changes

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -42,6 +42,34 @@ local plugins = {
     config = function()
       require("nvim-ts-autotag").setup()
     end
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+      config = function()
+    require('rainbow-delimiters.setup').setup {
+      strategy = {
+        [''] = require('rainbow-delimiters').strategy['global'],
+        vim = require('rainbow-delimiters').strategy['local'],
+      },
+      query = {
+        [''] = 'rainbow-delimiters',
+        lua = 'rainbow-blocks',
+      },
+      priority = {
+        [''] = 110,
+        lua = 210,
+      },
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+      },
+    }
+  end,
   }
 }
 
